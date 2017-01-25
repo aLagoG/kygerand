d634795a3464ba6b75b31bdb04c0afe7b9d2fb4f
Andres de Lago Gomez <delagoandres@gmail.com>
2017-01-25 13:52:33 -0600

    Create competitive.h
    first try at competitive.h

---
+++ competitive.h
@@ -0,0 +1,10 @@
+// Suppress some compilation warning messages (only for VC++ users)
+#define _CRT_SECURE_NO_DEPRECATE
+
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+#define INF 1000000000
