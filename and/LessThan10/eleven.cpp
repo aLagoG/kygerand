f0e6e1a134c45eac357eb87ed837c8a5a59071c5
aLagoG <delagoandres@gmail.com>
2017-01-27 17:41:50 -0600

    Initial commit for and

---
+++ and/LessThan10/eleven.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <stdio.h>
+#include <queue>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+inline int itoa(int n) {
+	return n + '0';
+}
+
+int main() {
+	//infijo a posfijo
+	stack<char> st;
+	string res;
+	char c;
+	while(scanf("%c",&c), c) {
+		
+	}
+}
