a4ca315e477022b0eec72d514e06404ee4fd5de4
kevinwkt <kevinwkt97@outlook.com>
2017-09-09 20:15:18 -0500

    L

--- icpc2017/3/B.cpp
+++ icpc2017/3/B.cpp
@@ -11,10 +11,10 @@ int main() {
         int* arr = (int *)malloc(sizeof(int) * n);
         int* lis = (int *)malloc(sizeof(int) * n);
         int* lis2 = (int *)malloc(sizeof(int) * n);
-        
+
         for(int i=0;i<n;i++){
             scanf("%d", &arr[i]);
-        } 
+        }
         int i, j, max = 0;
         for (i = 0; i < n; i++) lis[i] = 1;
         for (i = 1; i < n; i++){
@@ -22,21 +22,24 @@ int main() {
                 if (arr[i] >= arr[j] && lis[i] < lis[j] + 1)
                     lis[i] = lis[j] + 1;
         }
-        int indmax = 0;
+        // for(int k=0;k<n;k++){
+        //     printf("%d", lis[k]);
+        // }
+        // printf("\n\n");
         for (i = 0; i < n; i++){
-            for (int k = 0; k <= n; k++) lis2[k] = lis[i];
+            for (int k = 0; k < n; k++) lis2[k] = lis[i];
             for (int k = i+1; k < n; k++){
                 for (int z = i; z < k; z++)
                     if (arr[k] < arr[z] && lis2[k] < lis2[z] + 1)
-                        lis2[k] += lis2[z] + 1;
+                        lis2[k] = lis2[z] + 1;
             }
             for(int k=0;k<n;k++){
-                printf("%d", lis2[k);
+                // printf("%d", lis2[k]);
+                if (max < lis2[k]){
+                    max = lis2[k];
+                }
             }
-            printf("\n");
-            if (max < lis2[i]){
-                max = lis2[i];
-            }
+            // printf("\n");
         }
         free(lis);
         printf("%d\n", max);
