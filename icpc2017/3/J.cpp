958a01e4ec356292387f1095188fd31e75b58168
Andres de Lago <delagoandres@gmail.com>
2017-09-09 23:42:06 -0500

    Formated some cpp files and deleted the empty ones plus deleted a weird, probably mac related folder

--- icpc2017/3/J.cpp
+++ icpc2017/3/J.cpp
@@ -3,10 +3,10 @@
 
 using namespace std;
 
-int cnt(int i){
+int cnt(int i) {
     int r = 0;
-    while(i){
-        i &= (i-1);
+    while (i) {
+        i &= (i - 1);
         r++;
     }
     return r;
@@ -15,35 +15,36 @@ int cnt(int i){
 int main() {
     int t;
     scanf("%d", &t);
-    while(t--){
+    while (t--) {
         char w1[4];
         char w2[4];
-        scanf(" %c%c%c%c %c%c%c%c",&w1[0], &w1[1], &w1[2], &w1[3], &w2[0], &w2[1], &w2[2], &w2[3]);
+        scanf(" %c%c%c%c %c%c%c%c", &w1[0], &w1[1], &w1[2], &w1[3], &w2[0],
+              &w2[1], &w2[2], &w2[3]);
         stack<char> st;
         // 0 = i 1 = o
-        for(int i=0;i<=0xff;i++){
-            if(cnt(i)!=4){
+        for (int i = 0; i <= 0xff; i++) {
+            if (cnt(i) != 4) {
                 continue;
             }
             int idx = 0;
             int resIdx = 0;
             bool valid = 1;
 
-            for(int j=7; j>=0 && idx<4 && valid && resIdx<4 ;j--){
-                bool op = i&(1<<j);
-                if(op){
-                    if(st.top() != w2[resIdx]){
+            for (int j = 7; j >= 0 && idx < 4 && valid && resIdx < 4; j--) {
+                bool op = i & (1 << j);
+                if (op) {
+                    if (st.top() != w2[resIdx]) {
                         valid = 0;
                     }
                     st.pop();
-                }else{
+                } else {
                     st.push(w1[idx++]);
                 }
             }
-            if(valid){
-                printf(i&(1<<7)?"o":"i");
-                for(int j=1;j<8;j++){
-                    printf(i&(1<<j)?" o":" i");
+            if (valid) {
+                printf(i & (1 << 7) ? "o" : "i");
+                for (int j = 1; j < 8; j++) {
+                    printf(i & (1 << j) ? " o" : " i");
                 }
                 printf("\n");
             }
--- icpc2017/3/L.cpp
+++ icpc2017/3/L.cpp
@@ -3,56 +3,55 @@
 int arr[100001];
 
 int main() {
-  int n,k;
-  while(scanf("%d %d",&n,&k)!=EOF){
-    long sum=0;
-    int max=-1;
-    int max2=-1;
-    int window=0;
-    int stdsize=n/k;
-    for(int i=0;i<n;i++){
-      scanf("%d",&arr[i]);
-      sum+=arr[i];
-      if(i<stdsize){
-        window+=arr[i];
-      }else{
-        if(max2<window){
-          max2=window;
+    int n, k;
+    while (scanf("%d %d", &n, &k) != EOF) {
+        long sum = 0;
+        int max = -1;
+        int max2 = -1;
+        int window = 0;
+        int stdsize = n / k;
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+            sum += arr[i];
+            if (i < stdsize) {
+                window += arr[i];
+            } else {
+                if (max2 < window) {
+                    max2 = window;
+                }
+                window -= arr[i - stdsize];
+                window += arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
         }
-        window-=arr[i-stdsize];
-        window+=arr[i];
-      }
-      if(arr[i]>max){
-        max=arr[i];
-      }
-    }
-    int a=max,b=sum/k,c=max2;
-    while(1){
-      int gcount=1;
-      int localMax=-1;
-      int curr=0;
-      for(int i=0;i<n&&gcount<=k;i++){
-        if(curr+arr[i]<=b){
-          curr+=arr[i];
-        }else{
-          if(curr>localMax){
-            localMax=curr;
-          }
-          curr=arr[i];
-          gcount++;
+        int a = max, b = sum / k, c = max2;
+        while (1) {
+            int gcount = 1;
+            int localMax = -1;
+            int curr = 0;
+            for (int i = 0; i < n && gcount <= k; i++) {
+                if (curr + arr[i] <= b) {
+                    curr += arr[i];
+                } else {
+                    if (curr > localMax) {
+                        localMax = curr;
+                    }
+                    curr = arr[i];
+                    gcount++;
+                }
+            }
+            if (gcount < k) {
+                c = b;
+                b = (b - a) / 2 + a;
+            } else if (gcount > k) {
+                a = b;
+                b = (c - b) / 2 + b;
+            } else {
+                printf("%d\n", localMax);
+                break;
+            }
         }
-      }
-      if(gcount<k){
-        c=b;
-        b=(b-a)/2+a;
-      }else if(gcount>k){
-        a=b;
-        b=(c-b)/2+b;
-      }else{
-        printf("%d\n",localMax);
-        break;
-      }
     }
-
-  }
 }
